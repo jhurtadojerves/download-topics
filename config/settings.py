75fb1731585f15232b3ae598132f9181ea259a92
Julio Hurtado (Juliens) <juliohurtado.backend@gmail.com>
2021-03-22 09:04:00 -0500

    :bug: Fix styles

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -112,3 +113,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
